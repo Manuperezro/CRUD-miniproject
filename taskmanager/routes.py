d045839982ba972a320828c6a1e860fdbcd73354
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-04-17 11:57:26 +0000

    Add categories cards and buttons functions added

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -7,10 +7,11 @@ from taskmanager.models import Category, Task
 def home():
     return render_template("task.html")
 
-    
+
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
 
 
 @app.route("/add_category", methods=["GET", "POST"])
